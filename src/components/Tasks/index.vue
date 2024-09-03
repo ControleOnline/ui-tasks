583a08133f5b5186fa8bf7ffda9b81fdd0bcc071
Luiz Kim <luiz.kim@controleonline.com>
2024-09-03 14:16:52 -0300

    Update

--- src/components/Tasks/index.vue
+++ src/components/Tasks/index.vue
@@ -2,12 +2,12 @@
   <DefaultTable :configs="configs" v-if="configs" />
 </template>
 <script>
-import DefaultTable from "@controleonline/ui-default/src/components/Default/DefaultTable";
+
 import { mapActions, mapGetters } from "vuex";
 
 export default {
   components: {
-    DefaultTable,
+    
   },
   props: {
     context: {
