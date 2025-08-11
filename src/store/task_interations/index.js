c6a7bdc3e55ed3a7239333645feee4b650c67af2
JorgeCastroo <jorgeeduardo27007@gmail.com>
2025-08-11 18:44:52 -0300

    feat:v1

--- src/store/task_interations/index.js
+++ src/store/task_interations/index.js
@@ -1,16 +1,16 @@
-import * as actions from "@controleonline/ui-default/src/store/default/actions";
-import * as getters from "@controleonline/ui-default/src/store/default/getters";
-import mutations from "@controleonline/ui-default/src/store/default/mutations";
-import Formatter from "@controleonline/ui-common/src/utils/formatter.js";
+import * as actions from '@controleonline/ui-default/src/store/default/actions';
+import * as getters from '@controleonline/ui-default/src/store/default/getters';
+import mutations from '@controleonline/ui-default/src/store/default/mutations';
+import Formatter from '@controleonline/ui-common/src/utils/formatter.js';
 
 export default {
   namespaced: true,
   state: {
- item:{},
-items:[],
-    resourceEndpoint: "task_interations",
+    item: {},
+    items: [],
+    resourceEndpoint: 'task_interations',
     isLoading: false,
-    error: "",
+    error: '',
     violations: null,
     totalItems: 0,
     filters: {},
@@ -19,20 +19,20 @@ items:[],
         isIdentity: true,
         externalFilter: false,
         sortable: true,
-        name: "id",
-        align: "center",
-        label: "id",
+        name: 'id',
+        align: 'center',
+        label: 'id',
         format: function (value) {
-          return "#" + value;
+          return '#' + value;
         },
       },
       {
         externalFilter: false,
         sortable: true,
         editable: true,
-        name: "body",
-        align: "center",
-        label: "body",
+        name: 'body',
+        align: 'center',
+        label: 'body',
       },
     ],
   },
